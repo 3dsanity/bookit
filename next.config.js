11290d911832e3eb70baba12df480499626daa61
James <3dsanity@gmail.com>
2022-03-30 16:19:28 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    DB_LOCAL_URI: 'mongodb://localhost:27017/bookit',
-    CLOUDINARY_CLOUD_NAME: 'dutauawc3',
-    CLOUDINARY_API_KEY: '295565324278193',
-    CLOUDINARY_API_SECRET: 'NcDv32wxu9M7MtFYTS0x7pSYEBA',
-  },
+  env: {},
   images: {
     domains: ['res.cloudinary.com'],
   },
