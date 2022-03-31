86ab38996cae9d69e27c6a75b56ebb89a357015f
James <3dsanity@gmail.com>
2022-03-31 17:54:07 -0400

    add 404

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout/Layout';
+import NotFound from '../components/layout/NotFound';
+
+export default function NotFoundWrapper() {
+  return (
+    <>
+      <Layout title="Page Not Found">
+        <NotFound />
+      </Layout>
+    </>
+  );
+}
